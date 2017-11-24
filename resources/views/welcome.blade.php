c895696a7af43492620c5dd7c3f8744870345eb7
Eric Brian <eric.brianb12@gmail.com>
2017-11-24 16:18:07 -0200

    projeto copleto de login c/ steam

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #2c3e50;
+                color: white;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: white;
+                padding: 0 25px;
+                font-size: 16px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .links > a:hover{
+                text-decoration: underline;
+                color: black;
+                transition: 0.3s;
+            }
+
+        
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            img{
+                border-radius:60px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                        Laravel 
+                </div>
+
+                    @if(Auth::check())
+                        <img src="{{Auth::user()->avatar}}">
+                        <p style="font-weight: 600;">{{Auth::user()->username}}</p>
+                        <p  class="links"><a href={{'logout'}}>SAIR</a></p>
+                        
+                    @else
+                        <p class="links"><a href={{'auth/steam'}}>Entrar</a></p>
+                    @endif
+
+                
+            </div>
+        </div>
+    </body>
+</html>
